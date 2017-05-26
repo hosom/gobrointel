cdb222c4bc26b36094ac449caa1e3a94ad204ae5
Stephen Hosom <0xhosom@gmail.com>
2017-05-26 14:34:19 -0400

    Updates to support the new Bro intelligence types in 2.5. Added comments and fixes found with go lint.

--- brointel.go
+++ brointel.go
@@ -6,50 +6,69 @@ import (
     "strings"
 )
 
+// IndicatorType mapping for Bro intelligence framework
+// see the documentation for Intel::Type within Bro for more information
 type IndicatorType int
 
 const (
-    ADDR IndicatorType = iota
+	// An IP Address
+    Addr IndicatorType = iota
+	// A subnet in CIDR notation
+	Subnet
+	// A complete URL without the prefix
     URL
-    SOFTWARE
-    EMAIL
-    DOMAIN
-    USER_NAME
-    FILE_HASH
-    FILE_NAME
-    CERT_HASH
-    PUBKEY_HASH
+	// Software name
+    Software
+	// Email address
+    Email
+	// DNS domain name
+    Domain
+	// A user name
+    Username
+	// A non-hash type specific hash
+    FileHash
+	// A file name
+    FileName
+	// Certificate SHA-1 hash
+    CertHash
+	// Public Key MD5 hash
+    PubKeyHash
 )
 
-var HEADERS []string = []string{"#fields", "indicator", "indicator_type", "meta.source", "meta.desc", "meta.url", "meta.do_notice"}
+// headerFields contains the fields to be combined to make up a bro intel 
+// file header
+var headerFields = []string{"#fields", "indicator", "indicator_type", "meta.source", "meta.desc", "meta.url", "meta.do_notice"}
 
+// Headers returns a string containing Bro intelligence file headers
 func Headers() string {
-    return strings.Join(HEADERS, "\t")
+    return strings.Join(HeaderFields, "\t")
 }
 
 // String is used to cast an IndicatorType to a bro IndicatorType
 // this should prevent invalid input into the Bro Intel framework
 func (i IndicatorType) String() string {
     switch i {
-    case ADDR:
+    case Addr:
         return "Intel::ADDR"
+	case Subnet:
+		return "Intel::SUBNET"
     case URL:
         return "Intel::URL"
-    case SOFTWARE:
+    case Software:
         return "Intel::SOFTWARE"
-    case EMAIL:
+    case Email:
         return "Intel::EMAIL"
-    case DOMAIN:
+    case Domain:
         return "Intel::DOMAIN"
-    case USER_NAME:
+    case Username:
         return "Intel::USER_NAME"
-    case FILE_HASH:
+    case FileHash:
         return "Intel::FILE_HASH"
-    case FILE_NAME:
+    case FileName:
         return "Intel::FILE_NAME"
-    case CERT_HASH:
+    case CertHash:
         return "Intel::CERT_HASH"
-    case PUBKEY_HASH:
+    case PubKeyHash:
         return "Intel::PUBKEY_HASH"
     }
 
@@ -57,18 +76,20 @@ func (i IndicatorType) String() string {
     return "None"
 }
 
+// MetaData contains enrichment data about an Item
 type MetaData struct {
     // An arbirary string value representing the data source
     Source              string
     // Freeform description of the data
     Desc                string
     // A URL for more information about the data
-    Url                 string
+    URL                 string
     // Boolean value to allow the data itself to represent if the
     // indicator that this metadata is attached to is notice worthy 
     DoNotice            bool
 }
 
+// Item is a single piece of intelligence / observable
 type Item struct {
     // The intelligence indicator
     Indicator           string
